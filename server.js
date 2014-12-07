f05c950639d72c559e62f84c344e29cd238a8e98
Maya Driver <mayadrivermd@gmail.com>
2014-12-07 20:43:41 +0000

    linked ejs to json

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
-var fs = require('fs');
-var obj = JSON.parse(fs.readFileSync('./public/javascript/data.json', 'utf8'));
+var data = require('./public/javascript/data.json');
 
 app.use(require('express').static('public'));
 
@@ -10,7 +9,11 @@ app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response){
-  response.render('index', obj);
+  var result = [];
+for(var i in data){
+    result.push([i, data[i]]);}
+
+  response.render('index', {result: result});
 });
 
 http.listen(8001, function(){
